1674789c8138ef4ed8a6b9b5ecb4a2f8da1281fb
Lyova Potyomkin <lyova.potyomkin@gmail.com>
2025-08-15 16:16:18 +0300

    fix deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -14,24 +14,15 @@ import { ModularSmartAccount } from "src/ModularSmartAccount.sol";
 contract Deploy is Script {
     function run() public {
         // TODO: use correct owner address.
-        address owner = address(0x1234);
-        // TODO: use correct EOA account owner address.
-        address[] memory eoaOwners = new address[](1);
-        eoaOwners[0] = address(0x1234);
+        address owner = msg.sender;
 
         address[] memory defaultModules = new address[](3);
         defaultModules[0] = address(new TransparentUpgradeableProxy(address(new EOAKeyValidator()), owner, ""));
         defaultModules[1] = address(new TransparentUpgradeableProxy(address(new SessionKeyValidator()), owner, ""));
         defaultModules[2] = address(new TransparentUpgradeableProxy(address(new WebAuthnValidator()), owner, ""));
 
-        bytes[] memory initData = new bytes[](3);
-        initData[0] = abi.encode(eoaOwners);
-
         address accountImpl = address(new ModularSmartAccount());
         address beacon = address(new UpgradeableBeacon(accountImpl, owner));
-        MSAFactory factory = MSAFactory(address(new TransparentUpgradeableProxy(address(new MSAFactory(beacon)), owner, "")));
-
-        // TODO: use correct owner address.
-        // factory.transferOwnership(address(0x1234));
+        address factory = address(new TransparentUpgradeableProxy(address(new MSAFactory(beacon)), owner, ""));
     }
 }
